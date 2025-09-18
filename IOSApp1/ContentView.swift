474c9b64fd0a35b2b558ceac19d2548e2b64f974
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-09-18 09:39:03 -0400

    Initial Commit

---
+++ IOSApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSApp1
+//
+//  Created by Jose Flores on 2025-09-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSApp1/IOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp1App.swift
+//  IOSApp1
+//
+//  Created by Jose Flores on 2025-09-18.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
